033799ee106c459721e124a0fd9b82fb05861ff9
Memo <58190287+memo-567@users.noreply.github.com>
2022-12-03 10:53:35 +0100

    Upgrade to nodejs 18.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-NODEJS_VERSION="16.15.0"
+NODEJS_VERSION="18.0"
 
 # dependencies used by the app
 pkg_dependencies="ffmpeg postgresql"
