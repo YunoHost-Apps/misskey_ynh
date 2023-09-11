3e84414dcda379ddb129900e105367b4f07411b8
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-11 10:11:53 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-NODEJS_VERSION="18"
+NODEJS_VERSION="20"
 
 #=================================================
 # PERSONAL HELPERS
