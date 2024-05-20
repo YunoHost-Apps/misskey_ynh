5eded24470f8530e58d7a2fd996822fb7f32ea45
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-05-20 23:23:04 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,7 +13,7 @@ NODEJS_VERSION="20"
 misskey_build() {
     pushd "$install_dir"
         ynh_use_nodejs
-        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production yarn add ts-node webpack
+        ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production yarn add ts-node webpack -W
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production yarn build
         ynh_exec_warn_less ynh_exec_as "$app" env "$ynh_node_load_PATH" NODE_ENV=production yarn run init
     popd
